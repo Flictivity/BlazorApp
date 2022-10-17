4365ddcba1ae6c4cdbfcf119a9341f1720553cbe
Flictivity <bulat_sa1@mail.ru>
2022-10-17 19:24:34 +0300

    Create Blazor App with Add new "User" model Add new "UserService" service (added singleton) Add new "User info tables" page and component "user tables"

---
+++ BlazorApp/Model/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp.Model;
+
+public class User
+{
+    public string? FName { get; set; }
+    
+    public string? LName { get; set; }
+    
+    public int Age { get; set; }
+    
+    public string? City { get; set; }
+}
---
+++ BlazorApp/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using BlazorApp.Model;
+
+namespace BlazorApp.Services;
+
+public class UserService
+{
+    private static Random _random = new Random();
+    
+    private static readonly string[] Cityes = new[]
+    {
+        "Moscow", "Kazan", "Rio", "Tokyo", "New York", "Saint Petersburg", "Washington"
+    };
+    
+    private static readonly string[] FNames = new[]
+    {
+        "Ivan", "Bulat", "Nikita", "Roman", "Kamil", "Sergey", "Riyaz"
+    };
+    
+    private static readonly string[] LNames = new[]
+    {
+        "Ivanov", "Saifullin", "Petrov", "Vasiliev", "Sharipov", "Turushkin", "Zaripov"
+    };
+
+    public static List<User> GetUsersList()
+    {
+        return Enumerable.Range(1, _random.Next(0,6)).Select(index => new User()
+        {
+            FName = FNames[Random.Shared.Next(FNames.Length)],
+            LName = LNames[Random.Shared.Next(LNames.Length)],
+            Age = Random.Shared.Next(15, 55),
+            City = Cityes[Random.Shared.Next(Cityes.Length)]
+        }).ToList();
+    }
+}
