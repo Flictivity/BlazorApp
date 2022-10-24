de5498a7fd5103cab95378916db4fd0bfc20f2ba
Flictivity <bulat_sa1@mail.ru>
2022-10-24 19:26:53 +0300

    Add

---
+++ AuthorizationWebApp/AuthorizationWebApp/Data/UserService.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+
+namespace AuthorizationWebApp.Data;
+
+public class UserService
+{
+    public User currentUser = new User();
+    
+    private static readonly MongoClient client = new MongoClient("mongodb://localhost");
+    private static readonly IMongoDatabase db = client.GetDatabase("UsersDb");
+    private readonly IMongoCollection<User> usersCollection = db.GetCollection<User>("Users");
+    
+    public bool SaveUser(User user)
+    {
+        try
+        {
+            usersCollection.InsertOne(user);
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+    public User UserLogin(string Login)
+    {
+        try
+        {
+            currentUser = usersCollection.Find(x => x.Login == Login).FirstOrDefault();
+        }
+        catch (Exception e)
+        {
+        }
+
+        return currentUser;
+    }
+}
